85d52fc115e05abdc9a31e644fc9b191b86280e0
stefanovallarella <stefanovallarella@gmail.com>
2022-09-20 09:05:42 -0300

    useTodos added

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,21 @@
+import { TodoListItem } from "./TodoListItem";
+
+export const todoReducer = (initialState = [], action) => {
+  switch (action.type) {
+    case "[TODO] Add Todo":
+      return [...initialState, action.payload];
+    case "[TODO] Remove todo":
+      return initialState.filter((todo) => todo.id !== action.payload);
+    case "[TODO] Toggle todo":
+      return initialState.map((todo) => {
+        if (todo.id === action.payload) {
+          return { ...todo, done: !todo.done };
+        }
+
+        return todo;
+      });
+
+    default:
+      return initialState;
+  }
+};
